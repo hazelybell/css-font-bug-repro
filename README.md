67dcb4130af0a920bdc5defcc880e3d68ee0b4b4
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2023-11-22 07:15:15 -0700

    remove irrelevant

--- README.md
+++
@@ -1,5 +0,0 @@
-# slides-xt
-
-Refreshed reveal.js slides for CMPUT 404
-
-Uses webpack, reveal.js, nunjucks, and gh-pages.
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
 import vituum from 'vituum'
 import nunjucks from '@vituum/vite-plugin-nunjucks'
-import {prexample} from './src/scripts/prexample.js'
 
 export default {
-    // base: 'https://uofa-cmput404.github.io/slides-xt/',
-    base: '/slides-xt/',
+    base: './',
     build: {
-        minify: false,
         rollupOptions: {
             output: {
                 preserveModules: true,
-                compact: false,
-                minifyInternalExports: false,
             },
             preserveEntrySignatures: "exports-only",
         },
-        sourcemap: true,
-        target: 'esnext',
-        assetsInlineLimit: 0,
-    },
-    optimizeDeps: {
-        disabled: true,
     },
     plugins: [
         vituum({
@@ -30,9 +19,6 @@ export default {
         }),
         nunjucks({
             root: './src',
-            filters: {
-                prexample
-            },
             options: {
                 autoescape: true,
             }
